4393c7a21476f3cb42f11f03b664741a5fc81eef
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-21 20:26:05 +0200

    Updates

---
+++ precache-manifest.2550bf158e242e9ca9bebff5a212941b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-youtube-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1b42a2978684ff369b32",
+    "url": "/react-youtube-app/static/js/runtime~main.1b42a297.js"
+  },
+  {
+    "revision": "66c8fa77124e7c70c942",
+    "url": "/react-youtube-app/static/js/main.66c8fa77.chunk.js"
+  },
+  {
+    "revision": "74256310fb7041ab29b7",
+    "url": "/react-youtube-app/static/js/1.74256310.chunk.js"
+  },
+  {
+    "revision": "66c8fa77124e7c70c942",
+    "url": "/react-youtube-app/static/css/main.c9a7a38f.chunk.css"
+  },
+  {
+    "revision": "e9041c6a5276f4eeabe8d00a51732b74",
+    "url": "/react-youtube-app/index.html"
+  }
+];
